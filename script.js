a6d06fce664d7cc6c981db6696400d7d5a0bfe39
Ariel Lee <arielslove629@gmail.com>
2021-10-18 12:55:47 +0900

    Imported html, css, js files in

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Please don't delete the 'use strict' line above
+
+function contactMe() {
+  let firstName = prompt("What is your name?");
+  alert("Thank you " + firstName + ", I'll be in touch!");
+}
+
+//let contact = document.querySelector(".contactButton");
+//contact.addEventListener("click", contactMe);
